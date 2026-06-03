bdbf56a2e3007570e0e06e321d688ea88397be5f
Manikandan063 <manikandanthangaraj06@gmail.com>
2026-06-03 12:02:56 +0530

    updated backend

--- src/models/initModels.js
+++ src/models/initModels.js
@@ -13,45 +13,45 @@ import ParentSession from '../modules/parent/parentSession.model.js';
 
 const initModels = () => {
   // Parent - Student (One to Many)
-  Parent.hasMany(Student, { foreignKey: 'parentId', as: 'children' });
+  Parent.hasMany(Student, { foreignKey: 'parentId', as: 'children', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
   Student.belongsTo(Parent, { foreignKey: 'parentId', as: 'parent' });
 
   // Parent - ParentSession (One to Many)
-  Parent.hasMany(ParentSession, { foreignKey: 'parentId', as: 'sessions' });
+  Parent.hasMany(ParentSession, { foreignKey: 'parentId', as: 'sessions', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
   ParentSession.belongsTo(Parent, { foreignKey: 'parentId', as: 'parent' });
 
   // Student - Bus Transfer Logs (One to Many)
-  Student.hasMany(BusTransferLog, { foreignKey: 'studentId', as: 'transferLogs' });
+  Student.hasMany(BusTransferLog, { foreignKey: 'studentId', as: 'transferLogs', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
   BusTransferLog.belongsTo(Student, { foreignKey: 'studentId', as: 'student' });
   BusTransferLog.belongsTo(Bus, { foreignKey: 'oldBusId', as: 'oldBus' });
   BusTransferLog.belongsTo(Bus, { foreignKey: 'newBusId', as: 'newBus' });
 
   // School - Bus (One to Many)
-  School.hasMany(Bus, { foreignKey: 'schoolId', sourceKey: 'id', as: 'buses' });
+  School.hasMany(Bus, { foreignKey: 'schoolId', sourceKey: 'id', as: 'buses', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
   Bus.belongsTo(School, { foreignKey: 'schoolId', targetKey: 'id', as: 'school' });
 
   // Student - School (Many to One)
   Student.belongsTo(School, { foreignKey: 'schoolId', targetKey: 'id', as: 'school' });
-  School.hasMany(Student, { foreignKey: 'schoolId', sourceKey: 'id', as: 'students' });
+  School.hasMany(Student, { foreignKey: 'schoolId', sourceKey: 'id', as: 'students', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
 
   // Bus - BusLiveLocation (One to One)
-  Bus.hasOne(BusLiveLocation, { foreignKey: 'busId', as: 'liveLocation' });
+  Bus.hasOne(BusLiveLocation, { foreignKey: 'busId', as: 'liveLocation', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
   BusLiveLocation.belongsTo(Bus, { foreignKey: 'busId', as: 'bus' });
 
   // Student - Bus (Many to One)
   Student.belongsTo(Bus, { foreignKey: 'currentBusId', as: 'bus' });
-  Bus.hasMany(Student, { foreignKey: 'currentBusId', as: 'students' });
+  Bus.hasMany(Student, { foreignKey: 'currentBusId', as: 'students', onDelete: 'SET NULL', onUpdate: 'CASCADE' });
 
   // School - Admin (One to Many)
-  School.hasMany(Admin, { foreignKey: 'schoolId', sourceKey: 'id', as: 'admins' });
+  School.hasMany(Admin, { foreignKey: 'schoolId', sourceKey: 'id', as: 'admins', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
   Admin.belongsTo(School, { foreignKey: 'schoolId', targetKey: 'id', as: 'school' });
 
   // Parent - DismissedNotification (One to Many)
-  Parent.hasMany(DismissedNotification, { foreignKey: 'parentId', as: 'dismissedNotifications' });
+  Parent.hasMany(DismissedNotification, { foreignKey: 'parentId', as: 'dismissedNotifications', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
   DismissedNotification.belongsTo(Parent, { foreignKey: 'parentId', as: 'parent' });
 
   // Notification - DismissedNotification (One to Many)
-  Notification.hasMany(DismissedNotification, { foreignKey: 'notificationId', as: 'dismissals' });
+  Notification.hasMany(DismissedNotification, { foreignKey: 'notificationId', as: 'dismissals', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
   DismissedNotification.belongsTo(Notification, { foreignKey: 'notificationId', as: 'notification' });
 };
 
